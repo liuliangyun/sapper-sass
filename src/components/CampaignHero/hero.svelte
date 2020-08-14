260efafde40678eac0befada350a324b791c4eb2
lyliu <lyliu@thoughtworks.com>
2020-08-14 14:40:01 +0800

    add style for campaign hero svelte template

--- src/components/CampaignHero/hero.svelte
+++ src/components/CampaignHero/hero.svelte
@@ -2,15 +2,43 @@
   export let hero;
 </script>
 
+<style lang="scss">
+  @import './scss/main.scss';
+  
+  .hero {
+    background: #fff;
+    text-align: center;
+    a {
+      .img {
+        height: auto;
+        width: 100%;
+      }
+      &.tablet-hidden {
+        min-height: 200px;
+      }
+      &.mobile-hidden {
+        min-height: 300px;
+      }
+    }
+    .wrapper {
+      padding: 30px 0;
+    }
+    .description {
+      margin: 0;
+      padding: 0 20px;
+    }
+  }
+</style>
+
 <section class='hero relative'>
   {#if hero.url}
     <div class='block relative mobile-hidden'>
-      <a href={ hero.url }>
+      <a href='{ hero.url }'>
         <img src='{ hero.image_desktop }' alt='' class='img' />
       </a>
     </div>
     <div class='block relative show-mobile tablet-hidden'>
-      <a href={ hero.url }>
+      <a href='{ hero.url }'>
         <img src='{ hero.image_mobile }' alt='' class='img' />
       </a>
     </div>
@@ -24,7 +52,7 @@
   {/if}
   <div class='wrapper'>
     <h2>{ hero.title }</h2>
-    <p class='text description' style='padding:0 20px'>    
+    <p class='text description'>    
       {@html hero.article }
     </p>
   </div>
